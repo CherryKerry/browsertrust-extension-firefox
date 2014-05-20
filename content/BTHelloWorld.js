b5d9d3f42d5aecb18dfc186f81b9d0cb774ade69
D Win <dwin@wintech.net.nz>
2014-05-21 11:13:21 +1200

    HTTP Request to Python API in Hello World

--- content/BTHelloWorld.js
+++ content/BTHelloWorld.js
@@ -12,4 +12,11 @@ BTHelloWorld.sayHello = function(aEvent)
 	var stringBundle = document.getElementById("xulschoolhello-string-bundle");
     var message = stringBundle.getString("browsertrust.greeting.label");
     window.alert(message);
+    
+    alert('Test');
+        
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://103.250.233.28:8765/", false);
+    request.send(null);
+    alert(request.responseText);
 }
