292210d06d8c6923faeed5055c673a0cc33e9132
Cherry_Kerry <the.kerry.powell@gmail.com>
2014-05-26 07:37:17 +1200

    Browser Trust Engine version 1 complete, needs testing

--- content/BTEngine.js
+++ content/BTEngine.js
@@ -1,8 +1,30 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /*  			Browser Trust Fingerprint Engine | (c) Browser Trust 2014					      */
+/*										Version 1.0												  */
+/* 							this version has not been tested 									  */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-var BTEngine = {};  // Sha256 namespace
+var BTEngine = {};  // Browser Trust Engine or BTEngine namespace
+
+/**
+ * Method to be called to create a fingerprint for the currently active
+ * window in the browser
+ * 
+ * @return {Fingerprint} .hash is a SHA256 hash of the uri data
+ * 						 .uri is the uri of the hashed data
+ */
+BTEngine.calculateFingerprint = function()
+{
+	var html = BTEngine.getRootNode();
+	var uri = BTEngine.getCurrentURI();
+	var trustFile = BTEngine.getTrustFile(uri);
+	var htmlText = BTEngine.breakDownNodes(html);
+	var fingerprint = {
+		hash:Sha256.hash(htmlText),
+		uri	:uri
+	}
+	return fingerprint;
+}
 
 /**
  * Gets the root node of the current open window
@@ -28,15 +50,51 @@ BTEngine.getCurrentURI = function()
  * Breakdown the nodes and add them to a string
  * 
  * @param {Node} node to start from
+ * @param {String} parentName the node name of the parent
+ * @param {String[]} trustFile tells the file what text to add or ignore
  * @return {String} tagname and text content of each node
  */
-BTEngine.breakDownNodes = function(node) 
+BTEngine.breakDownNodes = function(node, parentName, trustFile) 
 {
 	var result = "";
 	for (var childNode = node.firstChild; childNode != null; childNode = childNode.nextSibling)
 	{	
-		result += childNode.tagName + childNode.textContent + "\n";
-		result += breakDownNodes(childNode);
+		var nodeName = parentName + "." + childNode.tagName;
+		if (BTEngine.shouldFingerprint(nodeName, trustFile)) 
+		{
+			result += childNode.textContent + "\n";
+		}
+		result += breakDownNodes(childNode, nodeName, trustFile);
 	}
 	return result;
+}
+
+/**
+ * Loads a trust file for a given url to an array
+ * 
+ * @param {String} uri to look for the file
+ * @return {String[]} an array key words
+ */
+BTEngine.getTrustFile = function(uri)
+{
+	//Load file from url
+	return {};
+}
+
+/**
+ * Caclulate if the fingerprint node should be finger printed
+ * 
+ * @param {String} nodeName path name of the node
+ * @param {String[]} trustFile the array to of path names to exclude
+ * @return {boolean} true if the node should be fingerprinted
+ */
+BTEngine.shouldFingerprint = function(nodeName, trustFile) 
+{
+	var shouldIgnore = false;
+	for (var i = 0; i < trustFile.length; i++) 
+	{
+		shouldIgnore = trustFile[i] == nodeName;
+	}
+	
+	return !shouldIgnore;
 }
