395ced99ee393ca5cd7954b2c265419b46f7c005
Tom Edgar <thomaswedgar@gmail.com>
2014-06-09 13:43:05 +1200

    Added code for storing Fingerprint localy, currently broken

--- content/BTrust.js
+++ content/BTrust.js
@@ -14,7 +14,6 @@ BrowserTrust.BTrust =
   getBTrustFile : function(aEvent) 
   {
 
-  	window.alert("Check for file /tmp/btrust.txt");
   	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 	file.initWithPath("/tmp/btrust.txt");
 	var wbp = Components.classes['@mozilla.org/embedding/browser/nsWebBrowserPersist;1'].createInstance(Components.interfaces.nsIWebBrowserPersist);
@@ -22,5 +21,7 @@ BrowserTrust.BTrust =
 	var uri = ios.newURI("https://www.google.com/robots.txt", null, null);
 	wbp.persistFlags &= ~Components.interfaces.nsIWebBrowserPersist.PERSIST_FLAGS_NO_CONVERSION; // don't save gzipped
 	wbp.saveURI(uri, null, null, null, null, file, null);
+	window.alert("Check for file /tmp/btrust.txt");
+	
   }
 };
