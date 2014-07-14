85077bffa6da49159dc39a2f106b1f1674e7383d
Cherry_Kerry <the.kerry.powell@gmail.com>
2014-07-14 16:57:26 +1200

    Refactor files and format all js to be similar formats

---
+++ content/test/BTTest.js
@@ -0,0 +1,29 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/*  					Browser Trust Test | (c) Browser Trust 2014							      */
+/*										Version 1.0												  */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+/**
+ * BrowserTrust namespace.
+ */
+if ("undefined" == typeof(BrowserTrust)) {
+  var BrowserTrust = {};
+};
+
+BrowserTrust.Test = 
+{
+    start : function()
+    {
+		var test = "";
+		//Hello World
+		test += "1) HelloWorld Test\n " + BrowserTrust.HelloWorld.getHello() + "\n";
+		//Cretae a fingerprint
+		var fingerprint = BrowserTrust.Engine.fingerprintHtml();
+		test += "2) Fingerprint Test\n URL: " + fingerprint.uri + "\n Fingerprint: " + fingerprint.hash + "\n"; 
+		//Browser Trust File
+		test += "3) Get BTrust File\n " + BrowserTrust.BTrust.getBTrustFile();
+		//Store fingerprint - Not working atm
+		//BrowserTrust.Storage.storeFingerprint(fingerprint);
+		alert(test);
+	}
+};
