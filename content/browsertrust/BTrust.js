8785c2e1b61f014e39cf554456fd6d29f828d553
Tom Edgar <thomaswedgar@gmail.com>
2014-07-16 14:29:44 +1200

    BTrust.js now fetches the url of the current page rather than a static url

--- content/browsertrust/BTrust.js
+++ content/browsertrust/BTrust.js
@@ -13,17 +13,18 @@ if ("undefined" == typeof(BrowserTrust)) {
 /**
  * Currently working on linux /tmp/btrust.txt not cross platform.
  * Calls the getBtrust function to get the .txt file from the server, then output something to show successful retrieval
- * Currently requires btrust.txt on webserver set up locally.
+ * Currently requires btrust.txt under root directory on webserver set up locally. CTRL+SHIFT+J to open console in FF
  */
 BrowserTrust.BTrust = 
 {
   	getBTrustFile : function(aEvent) 
   	{
+  		var location = window.content.location.protocol + window.content.location.hostname + "/btrust.txt";
 	  	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		file.initWithPath("/tmp/btrust.txt");
 		var wbp = Components.classes['@mozilla.org/embedding/browser/nsWebBrowserPersist;1'].createInstance(Components.interfaces.nsIWebBrowserPersist);
 		var ios = Components.classes['@mozilla.org/network/io-service;1'].getService(Components.interfaces.nsIIOService);
-		var uri = ios.newURI("http://localhost/btrust.txt", null, null);
+		var uri = ios.newURI(location, null, null);
 		wbp.persistFlags &= ~Components.interfaces.nsIWebBrowserPersist.PERSIST_FLAGS_NO_CONVERSION; // don't save gzipped
 		wbp.saveURI(uri, null, null, null, null, file, null);
 		return "Check for file /tmp/btrust.txt";
