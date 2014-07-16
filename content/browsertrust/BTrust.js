962adc2c3eb5de8c5d1646a0e09e8aa5949a0f6a
Tom Edgar <thomaswedgar@gmail.com>
2014-07-16 15:18:39 +1200

    Refactored US 31 Btrust.js to no longer save file to disk

--- content/browsertrust/BTrust.js
+++ content/browsertrust/BTrust.js
@@ -20,13 +20,37 @@ BrowserTrust.BTrust =
   	getBTrustFile : function(aEvent) 
   	{
   		var location = window.content.location.protocol + window.content.location.hostname + "/btrust.txt";
-	  	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+/*	Old method of saving file to disk  	
+ * var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
 		file.initWithPath("/tmp/btrust.txt");
 		var wbp = Components.classes['@mozilla.org/embedding/browser/nsWebBrowserPersist;1'].createInstance(Components.interfaces.nsIWebBrowserPersist);
 		var ios = Components.classes['@mozilla.org/network/io-service;1'].getService(Components.interfaces.nsIIOService);
 		var uri = ios.newURI(location, null, null);
 		wbp.persistFlags &= ~Components.interfaces.nsIWebBrowserPersist.PERSIST_FLAGS_NO_CONVERSION; // don't save gzipped
 		wbp.saveURI(uri, null, null, null, null, file, null);
-		return "Check for file /tmp/btrust.txt";
+		return "Check for file /tmp/btrust.txt";*/
+		
+  		//from http://stackoverflow.com/questions/19534908/firefox-extension-addon-reading-a-text-file-from-remote-serverurl
+  		
+		// Add XMLHttpRequest constructor, if not already present
+		if (!('XMLHttpRequest' in this)) {
+		    this.XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest");
+		}
+		
+		var req = new XMLHttpRequest();
+		req.open("GET", location); // file:/// would work too, BTW
+		req.overrideMimeType("text/plain");
+		req.addEventListener("load", function() {
+		  // Split the responseText into array to later use the urls in btrust.txt
+			var staticContent = req.responseText.split("/n");
+			for (var i = 0; i < staticContent.length; i++) {
+				console.log(staticContent[i]);
+			}
+		}, false);
+		req.addEventListener("error", function() {
+		  // Handle error
+		}, false);
+		req.send();
+		
   	}
 };
